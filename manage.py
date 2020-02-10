a145fb33ed3182d68f3390e26242542a4f8983e3
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-10 12:38:00 +0300

    Add Migrations for the database models | Add tests for User Model

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Role
 
 app = create_app('development')
@@ -7,6 +8,9 @@ test_app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('serve',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 # create a shel context
 @manager.shell
---
+++ tests/test_user.py
@@ -0,0 +1,33 @@
+import unittest
+from app.models import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class for the User Model
+    '''
+    
+    def setUp(self):
+        '''
+        Function to prepare for every test case
+        '''
+        self.new_user = User(password = 'kamau')
+
+    def test_password_setter(self):
+        '''
+        Test that the password setter works
+        '''
+        self.assertTrue(self.new_user.pass_secure is Not None)
+
+    def test_no_access_password(self):
+        '''
+        Confirm that attribute error is raised when we try to access password attrib
+        '''
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        '''
+        Test the verify_password function
+        '''
+        self.assertTrue(self.new_user.verify_password('kamau'))
+
